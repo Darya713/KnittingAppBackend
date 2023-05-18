4091ec8205058bff5757286aaf73ef8dd47336a5
Darya Krauchuk <Darya_Krauchuk@epam.com>
2023-05-18 16:52:42 +0200

    Add KnittingType controller

---
+++ Controllers/KnittingTypesController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using webapi.Models;
+
+namespace webapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class KnittingTypesController : ControllerBase
+{
+    private static readonly List<KnittingType> KnittingTypes = new()
+    {
+        new()
+        {
+            Id = "gloves",
+            ImageSource = "gloves.png",
+            Label = "Gloves"
+        },
+        new()
+        {
+            Id = "hat",
+            ImageSource = "hat.png",
+            Label = "Hat"
+        },
+        new()
+        {
+            Id = "scarf",
+            ImageSource = "scarf.png",
+            Label = "Scarf"
+        },
+        new()
+        {
+            Id = "socks",
+            ImageSource = "socks.png",
+            Label = "Socks"
+        },
+        new()
+        {
+            Id = "sweater",
+            ImageSource = "sweater.png",
+            Label = "Sweater"
+        }
+    };
+
+    private readonly ILogger<KnittingTypesController> _logger;
+
+    public KnittingTypesController(ILogger<KnittingTypesController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetKnittingTypes")]
+    public IEnumerable<KnittingType> Get()
+    {
+        return KnittingTypes.ToArray();
+    }
+}
---
+++ Models/KnittingType.cs
@@ -0,0 +1,10 @@
+namespace webapi.Models;
+
+public class KnittingType
+{
+    public string? Id { get; set; }
+
+    public string? ImageSource { get; set; }
+
+    public string? Label { get; set; }
+}
