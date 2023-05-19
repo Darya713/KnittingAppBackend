c2a61603b70015352fb80f93ba08afd0ac64c90a
Darya Krauchuk <Darya_Krauchuk@epam.com>
2023-05-19 10:29:02 +0200

    Add CORS

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+var MyAllowSpecificOrigins = "MyPolicy";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +7,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("https://knittingapp-backend.azurewebsites.net/knittingtypes/", "https://localhost:3000/")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+        });
+});
 
 var app = builder.Build();
 
@@ -17,6 +29,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
